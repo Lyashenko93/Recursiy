2f413cc1fe9bae4d67803df957b45445c8f6308e
Ирина Ляшенко <tawfuiofmv6s8z17h@mail.ru>
2024-04-11 01:48:43 +0300

    Решение задачи

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,20 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
- 
+
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
 int num1 = new Random().Next(1,10);
 int num2 = new Random().Next(10,20);
 Console.WriteLine("Сген. числа");
 
+PrintNum(num1, num2);
+
+void PrintNum(int start, int end)
+{
+    if(start > end)
+    {
+        return;
+    }
+    Console.Write(start+ " ");
+    PrintNum(++start, end);
+}
