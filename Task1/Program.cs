08d022eeb2b2d28594dc909091b1269b5c162369
Ирина Ляшенко <tawfuiofmv6s8z17h@mail.ru>
2024-04-11 02:37:58 +0300

    hh

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,5 @@
 ﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
-using System.Diagnostics.Contracts;
-using System.Runtime.InteropServices;
 
 int num1 = new Random().Next(1,10);
 int num2 = new Random().Next(10,20);
