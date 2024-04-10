72133d8c0ad5869d382f701675e9fa12e31d6982
Ирина Ляшенко <tawfuiofmv6s8z17h@mail.ru>
2024-04-11 02:57:17 +0300

    Решение задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,3 +6,25 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число");
 int n = Convert.ToInt32(Console.ReadLine());
 
+AkkermanFunction(m, n);
+
+void AkkermanFunction(int m, int n)
+{
+    Console.WriteLine(AkkermanFunction(m, n));
+}
+
+int Akkerman(int m, int n)
+{
+    if(n == 0)
+    {
+        return n+1;
+    }
+    else if(n ==0 && n > 0)
+    {
+        return Akkerman(n-1, 1);
+    }
+    else
+    {
+        return Akkerman(m-1, Akkerman(m, n - 1));
+    }
+}
